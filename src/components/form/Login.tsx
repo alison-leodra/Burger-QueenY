83cf38f1930ea67ce8691f6a8ffcacf650e735d6
alison-leodra <127059922+alison-leodra@users.noreply.github.com>
2023-07-31 18:50:03 -0400

    casi errores listos

--- src/components/form/Login.tsx
+++ src/components/form/Login.tsx
@@ -6,67 +6,86 @@ import axios from "axios"
 const Login = () => {
 
   const [user, setUser] = useState({
-    email:"",
-    password:""
+    email: "",
+    password: ""
   })
 
-  const submit = (e:any) => {
+  const [errors, setErrors] = useState({
+    email: false,
+    password: false
+  });
+
+  const submit = (e: any) => {
     e.preventDefault()
     axios.post('http://localhost:8080/login', user)
-    .then((data) => {
-      console.log(data.data)
-      console.log(data.data.user.role)
-      localStorage.setItem('tokenUser', data.data.token)
-    })
-    .catch((e) => {
-      console.error(e)
-    })
+      .then((data) => {
+        console.log(data.data)
+        console.log(data.data.user.role)
+        localStorage.setItem('tokenUser', data.data.token)
+        setErrors({
+          email: false,
+          password: false
+        });
+      })
+      .catch((e) => {
+        console.error(e)
+        setErrors({
+          email: true,
+          password: true
+        });
+      })
   }
 
-    return (
-        <>
-    <div className="login-container">
-      <img src={image} alt="logo-Hamburger"/>
-      <h1>Iniciar sesión</h1>
-      <form onSubmit={submit}>
-        <div className="field">
-          <label htmlFor="email">Correo Electrónico</label>
-          <input required 
-          onChange={(e) => {
-            setUser({
-              ...user,
-              email: e.target.value
-            })
-          }}
-          type="email" 
-          name="email" 
-          />
-        </div>
-        <div className="field">
-          <label htmlFor="password">Contraseña</label>
-          <input required 
-          onChange={(e) => {
-            setUser({
-              ...user,
-              password: e.target.value
-            })
-          }}
-          type="password" 
-          name="password"
-          />
-        </div>
-        <div className="submit">
-          <input
-          type="submit"
-          className="link"
-          placeholder="Enviar"
-        />
-        </div>
-      </form>
-            
-    </div>
+  return (
+    <>
+      <div className="login-container">
+        <img src={image} alt="logo-Hamburger" />
+        <h1>Iniciar sesión</h1>
+        <form onSubmit={submit}>
+          <div className="field">
+            <label htmlFor="email">Correo Electrónico</label>
+            <input required
+              onChange={(e) => {
+                setUser({
+                  ...user,
+                  email: e.target.value
+                })
+              }}
+              type="email"
+              name="email"
+            />
+            {errors.email && (
+              <p>Correo incorrecto</p>
+            )}
+          </div>
+          <div className="field">
+            <label htmlFor="password">Contraseña</label>
+            <input required
+              onChange={(e) => {
+                setUser({
+                  ...user,
+                  password: e.target.value
+                })
+              }}
+              type="password"
+              name="password"
+            />
+            {errors.password && (
+              <p>Contrasena incorrecta</p>
+            )}
+          </div>
+          <div className="submit">
+            <input
+              type="submit"
+              className="link"
+              placeholder="Enviar"
+            />
+          </div>
+        </form>
 
-</>
-    )
+      </div>
+
+    </>
+  )
 }
 export default Login
