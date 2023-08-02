99dd647f9457e63062a5eb6bc2ff7b2a5383a4fb
alison-leodra <127059922+alison-leodra@users.noreply.github.com>
2023-08-01 20:59:49 -0400

    login and waiter view

--- src/components/pages/waiter/PageWaiters.tsx
+++ src/components/pages/waiter/PageWaiters.tsx
@@ -1,45 +1,60 @@
 
 import "./PageWaiters.css"
 import product from "../../../images/Group 8(1).png"
+import AddOrder from "./AddOrder"
+import { useState } from "react";
 
 const PageWaiters = () => {
-    return (
-        <section className="container">
-            <div className="menuButtons">
-                <button className="button">DESAYUNO</button>
-                <button className="button">ALMUERZO Y CENA</button>
-            </div>
 
-            <div className="container-products">
-                <div className="product">
-                    <img className="img-products" src={product} alt="meals" />
-                    <p className="item-product">Café americano
-                        <span> $19</span></p>
-                </div>
-                <div className="product">
-                    <img className="img-products" src={product} alt="meals" />
-                    <p className="item-product">Café americano
-                        <span> $19</span></p>
-                </div>
-                <div className="product">
-                    <img className="img-products" src={product} alt="meals" />
-                    <p className="item-product">Café americano
-                        <span> $19</span></p>
-                </div>
-                <div className="product">
-                    <img className="img-products" src={product} alt="meals" />
-                    <p className="item-product">Café americano
-                        <span> $19</span></p>
-                </div>
-            </div>
-            <div className="menuButtons">
-                <input
-                    type="submit"
-                    className="button"
-                    value="VER CARRITO"
-                />
-            </div>
-        </section>
+    const [modal, setModal] = useState({ open: false })
+
+    const showModal = () => {
+        setModal({ open: true })
+    }
+
+    return (
+        <>
+            {!modal.open && (
+                <section className="container">
+                    <div className="menuButtons">
+                        <button className="button">DESAYUNO</button>
+                        <button className="button">ALMUERZO Y CENA</button>
+                    </div>
+
+                    <div className="container-products">
+                        <div className="product">
+                            <img className="img-products" src={product} alt="meals" />
+                            <p className="item-product">Café americano
+                                <span> $19</span></p>
+                        </div>
+                        <div className="product">
+                            <img className="img-products" src={product} alt="meals" />
+                            <p className="item-product">Café americano
+                                <span> $19</span></p>
+                        </div>
+                        <div className="product">
+                            <img className="img-products" src={product} alt="meals" />
+                            <p className="item-product">Café americano
+                                <span> $19</span></p>
+                        </div>
+                        <div className="product">
+                            <img className="img-products" src={product} alt="meals" />
+                            <p className="item-product">Café americano
+                                <span> $19</span></p>
+                        </div>
+                    </div>
+                    <div className="menuButtons">
+                        <input
+                            onClick={showModal}
+                            type="submit"
+                            className="button"
+                            value="VER CARRITO"
+                        />
+                    </div>
+                </section>
+            )} {modal.open && (<AddOrder />)}
+        </>
+
     )
 }
 
